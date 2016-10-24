404268bd624792e4cabf1a0037cb9a47f99a0b0f
Milagro Teruel <mteruel@zx81.famaf.unc.edu.ar>
2016-10-24 03:28:18 -0300

    Adding best defaults to svm baseline

--- experiments/svm_baseline.py
+++ experiments/svm_baseline.py
@@ -31,9 +31,20 @@ def main():
 
     if args['use_trees']:
         classifier = process_pipeline.get_basic_tree_pipeline(
-            ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-2,
+            ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3,
                                   n_iter=5, random_state=42)))
         parameters = process_pipeline.get_tree_parameter_grid()
+        features = process_pipeline.get_tree_steps()
+        classifier.set_params(**{  # Optimized for SVM
+            'features__ngrams__word_counter__max_features': 1000,
+            'features__ngrams__word_counter__ngram_range': (1, 1),
+            'features__transformer_list': [
+                ('ngrams', features['ngrams']),
+                ('pos_tags', features['pos_tags'])
+            ],
+        })
+        
+        # Grid parameters
         parameters['clf__alpha'] = (0.3, 0.1, 0.05, 1e-2, 1e-3)
     else:
         classifier = process_pipeline.get_basic_pipeline(
