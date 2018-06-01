45ba6bad28eb9011420e231600813b97c1d277c2
Milagro Teruel <milagro.teruel@gmail.com>
2018-06-01 06:42:08 -0300

    Changing the computation of f1 score for sklearn implementation

--- models/ArgBiLSTM.py
+++ models/ArgBiLSTM.py
@@ -2,6 +2,7 @@
 
 It has some more flexible functions, but the core of the model is the same."""
 
+from sklearn import metrics
 from ukplab_nets.neuralnets.BiLSTM import BiLSTM
 
 
@@ -10,3 +11,20 @@ class ArgBiLSTM(BiLSTM):
 
     def computeScore(self, modelName, devMatrix, testMatrix):
         return self.computeF1Scores(modelName, devMatrix, testMatrix)
+
+    def computeF1(self, modelName, sentences):
+        """Returns a traditional f1 score.
+
+        It does not check the consistency of BIO labels."""
+        labelKey = self.labelKeys[modelName]
+        model = self.models[modelName]
+        idx2Label = self.idx2Labels[modelName]
+
+        correctLabels = [sentences[idx][labelKey]
+                         for idx in range(len(sentences))]
+        predLabels = self.predictLabels(model, sentences)
+
+        pre, rec, f1, _ = metrics.precision_recall_fscore_support(
+            correctLabels, predLabels, average='weighted', warn_for=[])
+
+        return pre, rec, f1
