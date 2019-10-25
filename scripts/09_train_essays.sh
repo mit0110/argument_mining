c307269593e60f85318fa6d7ad70af419929ac9f
JohannaFrau <johannafrau18@gmail.com>
2019-10-25 09:08:40 -0300

    adding scripts

--- scripts/09_train_essays.sh
+++ scripts/09_train_essays.sh
@@ -1,11 +1,14 @@
-# Script to run the training in all configurations. Must be run from the
+
+# 
+
+#Script to run the training in all configurations. Must be run from the
 # argument mining folder
 
 DATE=$(date +%y-%m-%d-%H-%M)
-RESULT_DIRECTORY="/users/jfrau/am/results/essays2"
+RESULT_DIRECTORY="/home/jfrau/am/results/essays2.0"
 EXPERIMENT_DIRECTORY="ongoing-"$DATE
 SEPARATION_LEVEL="sentence"
-DATA_DIR="/users/mteruel/am/data/essays"
+DATA_DIR="/home/jfrau/am/data/processed_essays2.0"
 echo "******** Starting experiment $DATE"
 ATTENTION_MODEL=$7
 ATTENTION_ACTIVATION=$8
@@ -14,14 +17,14 @@ echo "******** Using attention " $ATTENTION_MODEL " " $ATTENTION_ACTIVATION
 mkdir $RESULT_DIRECTORY/
 mkdir $RESULT_DIRECTORY/$EXPERIMENT_DIRECTORY
 
-DATASET_NAME=$DATA_DIR/essays_komninos_e.p
+DATASET_NAME=$DATA_DIR/processed_essays2.0_komninos_e.p
 # The model resulting from this command will be saved in --output_dirpath + --experiment_name _model.h5
 python -u experiments/train_BiLSTM_CNN_CRF.py \
     --dataset $DATASET_NAME \
     --attention_model $ATTENTION_MODEL \
     --attention_activation $ATTENTION_ACTIVATION \
     --output_dirpath $RESULT_DIRECTORY/$EXPERIMENT_DIRECTORY \
-    --experiment_name essays \
+    --experiment_name essays2.0 \
     --char_embedding $1 \
     --char_embedding_size $2 \
     --epochs 50 \
@@ -37,7 +40,7 @@ python -u experiments/run_BiLSTM_CNN_CRF.py \
     --classifier $MODEL_NAME --dataset $DATASET_NAME \
     --attention_model $ATTENTION_MODEL \
     --output_dirname $RESULT_DIRECTORY/$EXPERIMENT_DIRECTORY \
-    --experiment_name essays
+    --experiment_name essays2.0
 
 echo "********** All experiments completed"
 mv $RESULT_DIRECTORY/$EXPERIMENT_DIRECTORY $RESULT_DIRECTORY/$DATE
